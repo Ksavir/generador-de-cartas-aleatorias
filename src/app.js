70d0b42dc6e30cccacc71b4cc21874282a1f53a3
Ksavir <kevinsavir20@gmail.com>
2022-07-02 14:53:05 +0000

    cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -5,7 +5,53 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+function randomCard() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let simboloAleatorio = () => {
+    let simbolo = ["♦", "♥", "♠", "♣"];
+    let simboloCarta = Math.floor(Math.random() * simbolo.length);
+    return simbolo[simboloCarta];
+  };
+
+  let pinta = simboloAleatorio();
+
+  if (pinta == "♦" || pinta == "♥") {
+    document.querySelector(".arriba").style = "color: red";
+    document.querySelector(".numeroCentral").style = "color: red";
+    document.querySelector(".abajo").style = "color: red";
+  } else {
+    document.querySelector(".arriba").style = "color: black";
+    document.querySelector(".numeroCentral").style = "color: black";
+    document.querySelector(".abajo").style = "color: black";
+  }
+
+  function numeroAleatorio() {
+    let numeros = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    var numeroCarta = Math.floor(Math.random() * numeros.length);
+    return numeros[numeroCarta];
+  }
+
+  let num = numeroAleatorio();
+
+  document.querySelector(".arriba").innerHTML = pinta;
+  document.querySelector(".numeroCentral").innerHTML = num;
+  document.querySelector(".abajo").innerHTML = pinta;
+}
+
+window.onload = function() {
+  randomCard();
 };
